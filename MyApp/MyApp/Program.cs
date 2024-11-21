1b6e6c8823d83cc536ec07100e7540f463fbfc17
Chipa010 <porovozova14@gmail.com>
2024-11-21 21:21:05 +0900

    Biblio

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MyLibrary; // Не забудьте добавить пространство имен вашей библиотеки
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            // Тестирование методов с типом void
+            myClass.PrintMessage("My first Bibleoteka");
+            myClass.AddNumbers(52, 887);
+
+            // Тестирование методов с возвращаемыми значениями
+            string greeting = myClass.GetGreeting("Dima");
+            Console.WriteLine(greeting);
+
+            int square = myClass.GetSquare(52);
+            Console.WriteLine($"Square of 52 is: {square}");
+
+            double area = myClass.GetCircleArea(222);
+            Console.WriteLine($"Area of circle with radius 5222 is: {area}");
+
+            // Ждем нажатия клавиши перед закрытием консоли
+            Console.ReadKey();
+        }
+    }
+}
